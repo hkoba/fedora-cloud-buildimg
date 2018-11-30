1db92159a5b35e021f6926fdec6ceda61f276b7c
Kobayasi, Hiroaki <buribullet@gmail.com>
2018-11-30 16:46:15 +0900

    Switch to ngompa/gce-oslogin

--- fedora-cloud-buildimg.tcl
+++ fedora-cloud-buildimg.tcl
@@ -23,6 +23,7 @@ snit::type fedora-cloud-buildimg {
     option -dry-run no
     option -verbose 0
     option -force no
+    option -keep-mount 0
 
     option -platform gcp
     option -mount-dir /mnt/tmp
@@ -33,11 +34,42 @@ snit::type fedora-cloud-buildimg {
     }
 
     #----------------------------------------
+    method build-from {srcXZFn {destRawFn ""}} {
+        set destRawFn [$self traced prepare-raw $srcXZFn $destRawFn]
+        set mountDir [$self traced mount-image $destRawFn]
+        $self traced install-to $mountDir
+        if {!$options(-keep-mount)} {
+            $self run exec umount $mountDir
+        }
+    }
+
+    #----------------------------------------
+    method prepare-raw {srcXZFn {destRawFn ""}} {
+        if {[file extension $srcXZFn] ne ".xz"
+            || [file extension [file rootname $srcXZFn]] ne ".raw"
+        } {
+            error "source image must be .raw.xz format"
+        }
+        if {$destRawFn eq ""} {
+            set destRawFn [$self image-name-for $srcXZFn]
+        }
+        $self run exec xzcat $srcXZFn > $destRawFn
+        set destRawFn
+    }
+    method image-name-for srcXZFn {
+        return $options(-platform)-[file rootname [file tail $srcXZFn]]
+    }
+
+    #----------------------------------------
     method install-to mountDir {
         $self run exec rsync -av [$self appdir]/sysroot/ $mountDir \
              >@ stdout 2>@ stderr
 
         $self run exec cp /etc/resolv.conf $mountDir/etc
+
+        $self run exec -ignorestderr chroot $mountDir \
+            dnf -y copr enable ngompa/gce-oslogin \
+            >@ stdout 2>@ stderr
 
         $self run exec -ignorestderr chroot $mountDir \
             dnf -vvvv install {*}[$self dnf-options]\
@@ -54,10 +86,12 @@ snit::type fedora-cloud-buildimg {
     #----------------------------------------
 
     method mount-image {diskImg {mountDir ""}} {
+        set mountDir [string-or $mountDir $options(-mount-dir)]
         $self run exec mount \
             -t auto \
             -o loop,offset=[$self read-start-offset $diskImg] \
-            $diskImg [string-or $mountDir $options(-mount-dir)]
+            $diskImg $mountDir
+        set mountDir
     }
 
     method read-start-offset {diskImg {partNo 0}} {
@@ -69,11 +103,18 @@ snit::type fedora-cloud-buildimg {
     }
 
     method read-partitions diskImg {
-        set json [exec sfdisk -J $diskImg]
+        set json [$self run exec sfdisk -J $diskImg]
         dict get [::json::json2dict $json] partitiontable partitions
     }
 
     #----------------------------------------
+    method traced args {
+        if {$options(-dry-run) || $options(-verbose)} {
+            puts "# self $args"
+        }
+        $self {*}$args
+    }
+
     method run {cmd args} {
         if {$options(-dry-run) || $options(-verbose)} {
             puts "# $cmd $args"
