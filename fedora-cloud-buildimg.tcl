3f2b1b2901942bd95a83d7d0d5554b4b625e59eb
Kobayasi, Hiroaki <buribullet@gmail.com>
2019-12-18 16:32:57 +0900

    Add image prepare $verNo

--- fedora-cloud-buildimg.tcl
+++ fedora-cloud-buildimg.tcl
@@ -10,6 +10,7 @@ exec tclsh -encoding utf-8 "$0" ${1+"$@"}
 package require snit
 package require fileutil
 package require json
+package require http
 
 namespace eval fedora-cloud-buildimg {
     ::variable realScriptFile [::fileutil::fullnormalize [info script]]
@@ -29,9 +30,56 @@ snit::type fedora-cloud-buildimg {
     option -platform gcp
     option -mount-dir /mnt/tmp
 
+    option -dist-url https://download.fedoraproject.org/pub/fedora/linux/releases/%d/Cloud/x86_64/images/
+
+    option -image-glob Fedora-Cloud-Base-*.raw.xz
     constructor args {
         $self configurelist $args
         set ::env(LANG) C
+    }
+
+    #========================================
+    method {image prepare} ver {
+        if {[set url_list [$self image list $ver]] eq ""} {
+            error "Can't find download url for Fedora $ver"
+        }
+        $self image download [lindex $url_list 0]
+    }
+
+    method {image list} {ver} {
+        set token [$self fetchURL [format $options(-dist-url) $ver]]
+        upvar #0 $token state
+        set baseUrl $state(url)
+        if {[http::ncode $token] != 200} {
+            error "Failed to fetch url($baseUrl): [http::data $token]"
+        }
+        set result []
+        foreach {- fn} [regexp -all -inline {<a href="([^\"]*)">} [http::data $token]] {
+            if {![string match $options(-image-glob) $fn]} continue
+            lappend result $baseUrl$fn
+        }
+        http::cleanup $token
+
+        set result
+    }
+    method {image download} url {
+        set fn [file tail $url]
+        if {![file exists $fn]
+            || ![$self confirm-yes "File $fn already exists. Reuse it? \[Y/n\] "]
+        } {
+            exec -ignorestderr curl -O $url 2>@ stderr
+        }
+        set fn
+    }
+
+    method fetchURL url {
+        set token [http::geturl $url]
+        while {[http::ncode $token] >= 300 && [http::ncode $token] < 400} {
+            set url [dict get [http::meta $token] location]
+            http::cleanup $token
+            set token [http::geturl $url]
+        }
+        set token
     }
 
     #========================================
@@ -159,6 +207,16 @@ snit::type fedora-cloud-buildimg {
             $cmd {*}$args
         }
     }
+
+    #----------------------------------------
+    # 改行時は yes と解釈する
+    method confirm-yes message {
+        puts -nonewline $message
+        flush stdout
+        gets stdin yn
+        expr {$yn eq "" || !!$yn}
+    }
+
     #----------------------------------------
     method appdir {} {
         return [file dirname [set ${type}::realScriptFile]]/$options(-platform)
